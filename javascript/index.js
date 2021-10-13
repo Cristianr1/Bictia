0c181ceec2434a22570054033e6463e449198be6
Cristian Romero <cristianrch123@gmail.com>
2021-10-13 18:07:19 -0500

    complete points

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,11 @@
-const { obtenerEtapaEducativa, reducer } = require('./utils');
+const {
+    obtenerEtapaEducativa,
+    obtenerGrado,
+    obtenerCurso,
+    reducer,
+    calcularMediana,
+    calcularModa
+} = require('./utils');
 
 
 /**
@@ -8,10 +15,11 @@ const { obtenerEtapaEducativa, reducer } = require('./utils');
  */
 function totalEstudiantesEtapa(etapaEducativa) {
     let totalEstudiantes = 0;
-    let mapEtapa = obtenerEtapaEducativa(etapaEducativa);
-    mapEtapa.forEach(grado => grado.map(curso => totalEstudiantes += curso.estudiantes.length));
+    let etapa = obtenerEtapaEducativa(etapaEducativa);
+    etapa.forEach(grado => grado.map(curso => totalEstudiantes += curso.estudiantes.length));
     return totalEstudiantes;
 };
+
 
 /**
  * Adiciona el total de estudiantes de las dos etapas educativas (primaria y secundaria)
@@ -21,6 +29,7 @@ function totalEstudiantesColegio() {
     return totalEstudiantesEtapa('primaria') + totalEstudiantesEtapa('secundaria');
 }
 
+
 /**
  * Calcula el número total de estudiantes en la etapa educativa, filtrando por el género
  * @param {string} etapaEducativa "primaria" | "secundaria"
@@ -29,13 +38,14 @@ function totalEstudiantesColegio() {
  */
 function totalPorGeneroEtapa(etapaEducativa, genero) {
     let totalEstudiantes = 0;
-    let mapEtapa = obtenerEtapaEducativa(etapaEducativa);
-    mapEtapa.forEach(grado => grado
+    let etapa = obtenerEtapaEducativa(etapaEducativa);
+    etapa.forEach(grado => grado
         .map(curso => curso.estudiantes
             .filter(estudiante => estudiante.genero === genero)
             .map(_estudiante => ++totalEstudiantes)));
     return totalEstudiantes;
 }
+
 
 /**
  * Adiciona el total de estudiantes de las dos etapas educativas (primaria y secundaria), filtrando por el género
@@ -46,6 +56,7 @@ function totalPorGeneroColegio(genero) {
     return totalPorGeneroEtapa('primaria', genero) + totalPorGeneroEtapa('secundaria', genero);
 }
 
+
 /**
  * Retorna la media de las notas, filtrando por etapa educativa
  * @param {string} etapaEducativa "primaria" | "secundaria"
@@ -55,7 +66,7 @@ function mediaNotasEtapa(etapaEducativa) {
     let sumNotas = 0;
     let totalNotas = 0;
     let etapa = obtenerEtapaEducativa(etapaEducativa);
-    etapa.forEach((_grado, index) => etapa.get(index)
+    etapa.forEach(grado => grado
         .map(curso => curso.estudiantes
             .map(estudiante => estudiante.asignaturas
                 .map(asignatura => {
@@ -67,6 +78,7 @@ function mediaNotasEtapa(etapaEducativa) {
     )
     return (sumNotas / totalNotas)
 }
+
 
 /**
  * Retorna la media de las notas en el colegio
@@ -76,93 +88,280 @@ function mediaNotasColegio() {
     return (mediaNotasEtapa('primaria') + mediaNotasEtapa('secundaria')) / 2;
 }
 
+
 /**
- * Retorna la media de las notas, filtrando por etapa educativa y grado
+ * Retorna la media de las notas, filtrando por: etapa educativa y grado
  * @param {string} etapaEducativa "primaria" | "secundaria"
  * @param {string} grado
  * @returns {number}
  */
-function mediaNotasEtapaGrado(etapaEducativa, grado) {
+function mediaNotasEtapaGrado(etapaEducativa, gradoFiltro) {
     let sumNotas = 0;
     let totalNotas = 0;
     let etapa = obtenerEtapaEducativa(etapaEducativa);
-    etapa.forEach((_gra, index) => index === grado && etapa.get(index)
-        .map(curso => curso.estudiantes
-            .map(estudiante => estudiante.asignaturas
-                .map(asignatura => {
-                    sumNotas += Object.values(asignatura[Object.keys(asignatura)]).reduce(reducer);
-                    totalNotas += Object.values(asignatura[Object.keys(asignatura)]).length;
-                })
-            )
+    let grado = obtenerGrado(etapa, gradoFiltro);
+
+    grado.map(curso => curso.estudiantes
+        .map(estudiante => estudiante.asignaturas
+            .map(asignatura => {
+                sumNotas += Object.values(asignatura[Object.keys(asignatura)]).reduce(reducer);
+                totalNotas += Object.values(asignatura[Object.keys(asignatura)]).length;
+            })
         )
     )
     return (sumNotas / totalNotas)
 }
 
+
 /**
- * Función 6
- * Retorna la media de las notas, filtrando por etapa educativa, grado y curso
+ * Retorna la media de las notas, filtrando por: etapa educativa, grado y curso
  * @param {string} etapaEducativa "primaria" | "secundaria"
  * @returns {number}
  */
-function mediaNotasEtapaCurso(etapaEducativa, grado, cursoFiltro) {
+function mediaNotasEtapaCurso(etapaEducativa, gradoFiltro, cursoFiltro) {
     let sumNotas = 0;
     let totalNotas = 0;
     let etapa = obtenerEtapaEducativa(etapaEducativa);
-    etapa.forEach((_gra, index) => index === grado && etapa.get(index)
-        .map(curso => curso.curso === cursoFiltro && curso.estudiantes
-            .map(estudiante => estudiante.asignaturas
-                .map(asignatura => {
-                    sumNotas += Object.values(asignatura[Object.keys(asignatura)]).reduce(reducer);
-                    totalNotas += Object.values(asignatura[Object.keys(asignatura)]).length;
-                })
-            )
-        )
+    let estudiantes = obtenerCurso(etapa, gradoFiltro, cursoFiltro);
+    estudiantes.map(estudiante => estudiante.asignaturas
+        .map(asignatura => {
+            sumNotas += Object.values(asignatura[Object.keys(asignatura)]).reduce(reducer);
+            totalNotas += Object.values(asignatura[Object.keys(asignatura)]).length;
+        })
     )
     return (sumNotas / totalNotas)
 }
 
+
 /**
- * Retorna un arreglo, en la primer posición está el nombre del estudiante con mayor puntaje,
- * en la segunda posición está el puntaje obtenido por el estudiante.
+ * Extrae las notas de una etapa en un arreglo, para así calcular mediana y moda.
+ * @param {string} etapaEducativa "primaria" | "secundaria"
+ * @returns {Object} Arreglo de notas, mediana y moda
+ */
+function notasEtapa(etapaEducativa) {
+    let arregloNotas = [];
+    let etapa = obtenerEtapaEducativa(etapaEducativa);
+    etapa.forEach(grado => grado
+        .map(curso => curso.estudiantes
+            .map(estudiante => estudiante.asignaturas
+                .map(asignatura => Object.values(asignatura[Object.keys(asignatura)]).forEach(corte => arregloNotas.push(corte)))
+            )
+        )
+    )
+    let mediana = calcularMediana(arregloNotas);
+    let moda = calcularModa(arregloNotas);
+    return { arregloNotas, mediana, moda };
+}
+
+
+/**
+ * Extrae las notas del colegio en un arreglo, para así calcular mediana y moda.
+ * @returns {Object} mediana y moda
+ */
+function notasColegio() {
+    let arregloNotasColegio = notasEtapa('primaria').arregloNotas.concat(notasEtapa('secundaria').arregloNotas);
+    let mediana = calcularMediana(arregloNotasColegio);
+    let moda = calcularModa(arregloNotasColegio);
+    return { mediana, moda };
+}
+
+/**
+ * Extrae las notas de un grado en un arreglo, para así calcular mediana y moda.
+ * @param {string} etapaEducativa "primaria" | "secundaria"
+ * @param {string} gradoFiltro grado
+ * @returns {Object} mediana y moda
+ */
+function notasEtapaGrado(etapaEducativa, gradoFiltro) {
+    let arregloNotas = [];
+    let etapa = obtenerEtapaEducativa(etapaEducativa);
+    let grado = obtenerGrado(etapa, gradoFiltro);
+    grado.map(curso => curso.estudiantes
+        .map(estudiante => estudiante.asignaturas
+            .map(asignatura => Object.values(asignatura[Object.keys(asignatura)]).forEach(corte => arregloNotas.push(corte)))
+        )
+    )
+    let mediana = calcularMediana(arregloNotas);
+    let moda = calcularModa(arregloNotas);
+    return { mediana, moda };
+}
+
+/**
+ * Extrae las notas de un curso en un arreglo, para así calcular mediana y moda.
+ * @param {string} etapaEducativa "primaria" | "secundaria"
+ * @param {string} gradoFiltro grado
+ * @param {string} cursoFiltro curso
+ * @returns {Object} mediana y moda
+ */
+function notasEtapaCurso(etapaEducativa, gradoFiltro, cursoFiltro) {
+    let arregloNotas = [];
+    let etapa = obtenerEtapaEducativa(etapaEducativa);
+    let estudiantes = obtenerCurso(etapa, gradoFiltro, cursoFiltro);
+    estudiantes.map(estudiante => estudiante.asignaturas
+        .map(asignatura => Object.values(asignatura[Object.keys(asignatura)]).forEach(corte => arregloNotas.push(corte)))
+    )
+    let mediana = calcularMediana(arregloNotas);
+    let moda = calcularModa(arregloNotas);
+    return { mediana, moda };
+}
+
+/**
+ * Calcula la media más alta en una materia y respecto a una etapa
  * @param {string} etapaEducativa "primaria" | "secundaria"
  * @param {string} materia
- * @returns {number}
+ * @returns {object} { nombre: estudianteMayorPuntaje, promedio: mayorPuntaje }
  */
 function mejorPromedioMateriaEtapa(etapaEducativa, materia) {
     let mayorPuntaje = 0;
     let estudianteMayorPuntaje = null;
     let etapa = obtenerEtapaEducativa(etapaEducativa);
-    etapa.forEach((_grado, index) => {
-        etapa.get(index)
-            .map(curso => curso.estudiantes
-                .map(estudiante => {
-                    const promedioAsignaturas = estudiante.asignaturas
-                        .map(asignatura => ({ [Object.keys(asignatura)]: Object.values(asignatura[Object.keys(asignatura)]).reduce(reducer) / 4.0 }))
-                    return { estudiante: estudiante.nombre, asignaturas: [...promedioAsignaturas] };
-                })
-                .map(nuevoArreglo => [nuevoArreglo.estudiante, nuevoArreglo.asignaturas.find(asignatura => asignatura[materia])[materia]])
-                .forEach(nuevoArreglo2 => {
-                    if (nuevoArreglo2[1] > mayorPuntaje) {
-                        estudianteMayorPuntaje = nuevoArreglo2[0];
-                        mayorPuntaje = nuevoArreglo2[1];
-                    }
-                })
-            )
-    })
-    return [estudianteMayorPuntaje, mayorPuntaje];
+    etapa.forEach(grado => grado.map(curso => curso.estudiantes
+        .map(estudiante => {
+            const promedioEstudiante = estudiante.asignaturas
+                .filter(asignatura => asignatura[materia])
+                .map(asignatura => Object.values(asignatura[materia]).reduce(reducer) / Object.values(asignatura[materia]).length);
+            if (promedioEstudiante[0] > mayorPuntaje) {
+                mayorPuntaje = promedioEstudiante[0];
+                estudianteMayorPuntaje = estudiante.nombre;
+            }
+        })
+    ));
+    return { nombre: estudianteMayorPuntaje, promedio: mayorPuntaje };
+};
+
+/**
+ * Calcula la media más alta en una materia
+ * @param {string} materia
+ * @returns {Object} { nombre: estudianteMayorPuntaje, promedio: mayorPuntaje }
+ */
+function mejorPromedioMateria(materia) {
+    if (mejorPromedioMateriaEtapa('primaria', materia).promedio > mejorPromedioMateriaEtapa('secundaria', materia).promedio) {
+        return mejorPromedioMateriaEtapa('primaria', materia);
+    }
+    return mejorPromedioMateriaEtapa('secundaria', materia);
 }
 
 /**
- * Retorna la media de las notas en el colegio
- * @param {string} materia
- * @returns {number}
+ * Calcula la media más alta en la etapa
+ * @param {string} etapaEducativa "primaria" | "secundaria"
+ * @returns {Object} { nombre: estudianteMayorPuntaje, promedio: mayorPuntaje }
  */
- function mejorPromedioMateria(materia) {
-    if (mejorPromedioMateriaEtapa('primaria', materia)[1] > mejorPromedioMateriaEtapa('secundaria', materia)[1]){
-        return mejorPromedioMateriaEtapa('primaria', materia);
-    }
-    return mejorPromedioMateriaEtapa('secundaria', materia)[1];
+function mejorPromedioEtapa(etapaEducativa) {
+    let estudianteMayorPuntaje = null;
+    let mayorPuntaje = null;
+    let etapa = obtenerEtapaEducativa(etapaEducativa);
+    etapa.forEach(grado => grado.map(curso => curso.estudiantes
+        .map(estudiante => {
+            const promedioEstudiante = estudiante.asignaturas.map(asignatura => {
+                let promedioAsignatura = 0;
+                const notasAsignatura = Object.values(asignatura[Object.keys(asignatura)]);
+                promedioAsignatura += notasAsignatura.reduce(reducer) / notasAsignatura.length;
+                return promedioAsignatura;
+            }).reduce(reducer) / estudiante.asignaturas.length;
+            if (promedioEstudiante > mayorPuntaje) {
+                mayorPuntaje = promedioEstudiante;
+                estudianteMayorPuntaje = estudiante.nombre;
+            }
+        })
+    ));
+    return { nombre: estudianteMayorPuntaje, promedio: mayorPuntaje };
+};
+
+/**
+ * Calcula la media más alta en un grado
+ * @param {string} etapaEducativa "primaria" | "secundaria"
+ * @param {string} gradoFiltro grado
+ * @returns {Object} { nombre: estudianteMayorPuntaje, promedio: mayorPuntaje }
+ */
+function mejorPromedioEtapaGrado(etapaEducativa, gradoFiltro) {
+    let estudianteMayorPuntaje = null;
+    let mayorPuntaje = null;
+    let etapa = obtenerEtapaEducativa(etapaEducativa);
+    let grado = obtenerGrado(etapa, gradoFiltro);
+    grado.map(curso => curso.estudiantes
+        .map(estudiante => {
+            const promedioEstudiante = estudiante.asignaturas.map(asignatura => {
+                let promedioAsignatura = 0;
+                const notasAsignatura = Object.values(asignatura[Object.keys(asignatura)]);
+                promedioAsignatura += notasAsignatura.reduce(reducer) / notasAsignatura.length;
+                return promedioAsignatura;
+            }).reduce(reducer) / estudiante.asignaturas.length;
+            if (promedioEstudiante > mayorPuntaje) {
+                mayorPuntaje = promedioEstudiante;
+                estudianteMayorPuntaje = estudiante.nombre;
+            }
+        })
+    );
+    return { nombre: estudianteMayorPuntaje, promedio: mayorPuntaje };
+};
+
+
+/**
+ * Calcula la media más alta en un curso
+ * @param {string} etapaEducativa "primaria" | "secundaria"
+ * @param {string} gradoFiltro grado
+ * @param {string} cursoFiltro curso
+ * @returns {Object} { nombre: estudianteMayorPuntaje, promedio: mayorPuntaje }
+ */
+function mejorPromedioEtapaCurso(etapaEducativa, gradoFiltro, cursoFiltro) {
+    let estudianteMayorPuntaje = null;
+    let mayorPuntaje = null;
+    let etapa = obtenerEtapaEducativa(etapaEducativa);
+    let estudiantes = obtenerCurso(etapa, gradoFiltro, cursoFiltro);
+    estudiantes.map(estudiante => {
+        const promedioEstudiante = estudiante.asignaturas.map(asignatura => {
+            let promedioAsignatura = 0;
+            promedioAsignatura += Object.values(asignatura[Object.keys(asignatura)]).reduce(reducer)
+            return promedioAsignatura;
+        }).reduce(reducer);
+        if (promedioEstudiante > mayorPuntaje) {
+            mayorPuntaje = promedioEstudiante;
+            estudianteMayorPuntaje = estudiante.nombre;
+        }
+    });
+    return { nombre: estudianteMayorPuntaje, promedio: mayorPuntaje };
+};
+
+
+/**
+ * Calcula la media más alta en el colegio
+ * @param {string} etapaEducativa "primaria" | "secundaria"
+ * @param {string} gradoFiltro grado
+ * @returns {Object} { nombre: estudianteMayorPuntaje, promedio: mayorPuntaje }
+ */
+function mejorPromedioColegio() {
+    const mejorPromedioPrimaria = mejorPromedioEtapa('primaria');
+    const mejorPromedioSecundaria = mejorPromedioEtapa('secundaria');
+    if (mejorPromedioPrimaria.promedio > mejorPromedioSecundaria.promedio) return mejorPromedioPrimaria;
+    return mejorPromedioSecundaria
 }
 
-console.log(mejorPromedioMateria('historia'))
+console.log(`La cantidad total de estudiantes que hay en el colegio es: ${totalEstudiantesColegio()}`);
+console.log(`La cantidad total de estudiantes que hay en primaria es: ${totalEstudiantesEtapa('primaria')}`);
+console.log(`La cantidad total de estudiantes que hay en secundaria es: ${totalEstudiantesEtapa('secundaria')}`);
+console.log(`La cantidad de niños que hay en el colegio es: ${totalPorGeneroColegio('male')}`);
+console.log(`La cantidad de niñas que hay en el colegio es: ${totalPorGeneroColegio('female')}`);
+console.log(`La cantidad de niños que hay en primaria es: ${totalPorGeneroEtapa('primaria', 'male')}`);
+console.log(`La cantidad de niñas que hay en primaria es: ${totalPorGeneroEtapa('primaria', 'female')}`);
+console.log(`La cantidad de niños que hay en secundaria es: ${totalPorGeneroEtapa('secundaria', 'male')}`);
+console.log(`La cantidad de niñas que hay en secundaria es: ${totalPorGeneroEtapa('secundaria', 'female')}`);
+console.log(`La media de las notas en el colegio es: ${mediaNotasColegio()}`);
+console.log(`La media de las notas en el bachillerato es: ${mediaNotasEtapa('secundaria')}`);
+console.log(`La media de las notas en la primaria es: ${mediaNotasEtapa('primaria')}`);
+console.log(`La media de las notas de un grado seleccionado por parametro (grado segundo): ${mediaNotasEtapaGrado('primaria', 'segundo')}`);
+console.log(`La media de las notas de un curso seleccionado por parametro (grado segundo): ${mediaNotasEtapaCurso('primaria', 'segundo', 'A')}`);
+console.log(`La moda de las notas en el colegio: ${notasColegio().moda}`);
+console.log(`La moda de las notas en el bachillerato: ${notasEtapa('secundaria').moda}`);
+console.log(`La moda de las notas en la primaria: ${notasEtapa('primaria').moda}`);
+console.log(`La moda de las notas de un grado seleccionado por parametro (grado tercero): ${notasEtapaGrado('primaria', 'tercero').moda}`);
+console.log(`La moda de las notas de un curso seleccionado por parametro (grado tercero B): ${notasEtapaCurso('primaria', 'tercero', 'B').moda}`);
+console.log(`La mediana de las notas en el colegio: ${notasColegio().mediana}`);
+console.log(`La mediana de las notas en el bachillerato: ${notasEtapa('secundaria').mediana}`);
+console.log(`La mediana de las notas en la primaria: ${notasEtapa('primaria').mediana}`);
+console.log(`La mediana de las notas de un grado seleccionado por parametro (grado tercero): ${notasEtapaGrado('primaria', 'tercero').mediana}`);
+console.log(`La mediana de las notas de un curso seleccionado por parametro (grado tercero B): ${notasEtapaCurso('primaria', 'tercero', 'B').mediana}`);
+console.log(`El estudiante con mejor nota en promedio en cada materia (fisica): ${mejorPromedioMateria('fisica').nombre}`);
+console.log(`El estudiante con mejor nota en promedio en el curso (grado segundo B): ${mejorPromedioEtapaCurso('primaria', 'segundo', 'B').nombre}`);
+console.log(`El estudiante con mejor nota en promedio en el grado (grado segundo): ${mejorPromedioEtapaGrado('primaria', 'segundo').nombre}`);
+console.log(`El estudiante con mejor nota en promedio en primaria: ${mejorPromedioEtapa('primaria').nombre}`);
+console.log(`El estudiante con mejor nota en promedio en bachillerato: ${mejorPromedioEtapa('secundaria').nombre}`);
+console.log(`El estudiante con mejor nota en promedio en el colegio: ${mejorPromedioColegio().nombre}`);
--- javascript/utils.js
+++ javascript/utils.js
@@ -14,4 +14,61 @@ function obtenerEtapaEducativa(etapaEducativa) {
 
 const reducer = (previousValue, currentValue) => previousValue + currentValue;
 
-module.exports = { obtenerEtapaEducativa, reducer }
+function obtenerGrado(mapEtapa, gradoFiltro){
+    let gradoArray;
+    mapEtapa.forEach((grado, index) => {
+        if(index === gradoFiltro) {
+            gradoArray = grado;
+    }
+    });
+    return gradoArray;
+}
+
+function obtenerCurso(mapEtapa, gradoFiltro, cursoFiltro){
+    let grado = obtenerGrado(mapEtapa, gradoFiltro);
+    return grado.find(curso => curso.curso === cursoFiltro).estudiantes;
+}
+
+/**
+ * Función que calcula la mediana
+ * @param {Array} notas arreglo de notas.
+ * @return {number} La mediana de las notas.
+ */
+function calcularMediana(notas) {
+    notas.sort((a, b) => a - b);
+    let mediana = null;
+    if(notas.length % 2 === 0) {
+        mediana = (notas[notas.length/2 - 1] + notas[notas.length/2]) / 2.0;
+    } else {
+        mediana = notas[(notas.length-1)/2];
+    }
+    return mediana;
+}
+
+/**
+ * Función que calcula la moda
+ * @param {Array} notas arreglo de notas.
+ * @return {Array} La moda de las notas
+ */
+ function calcularModa(notas) {
+    let modas = [], count = [], i, numero, indiceMax = 0;
+ 
+    for (i = 0; i < notas.length; i++) {
+        numero = notas[i];
+        count[numero] = (count[numero] || 0) + 1;
+        if (count[numero] > indiceMax) {
+            indiceMax = count[numero];
+        }
+    }
+ 
+    for (i in count)
+        if (count.hasOwnProperty(i)) {
+            if (count[i] === indiceMax) {
+                modas.push(Number(i));
+            }
+        }
+ 
+    return modas;
+}
+
+module.exports = { obtenerEtapaEducativa, obtenerGrado, obtenerCurso, reducer, calcularMediana, calcularModa }
